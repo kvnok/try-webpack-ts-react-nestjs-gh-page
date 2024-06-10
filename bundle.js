4a335d7fc18b41a43bc9904083f1d1663834427b
Kevin Kroon <kkroon@student.codam.nl>
2024-06-10 14:50:16 +0200

    Updates

--- bundle.js
+++ bundle.js
@@ -76,7 +76,7 @@ eval("\n\nif (false) {} else {\n  module.exports = __webpack_require__(/*! ./cjs
   \***************************************/
 /***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
-eval("\nvar __importDefault = (this && this.__importDefault) || function (mod) {\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\n};\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\nconst react_1 = __importDefault(__webpack_require__(/*! react */ \"./node_modules/react/index.js\"));\nconst App = () => {\n    return (react_1.default.createElement(\"div\", null,\n        react_1.default.createElement(\"h1\", null, \"Welcome to Odin Restaurant Page\")));\n};\nexports[\"default\"] = App;\n\n\n//# sourceURL=webpack://try-webpack-ts-react-nestjs-gh-page/./src/client/components/App.tsx?");
+eval("\nvar __importDefault = (this && this.__importDefault) || function (mod) {\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\n};\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\nconst react_1 = __importDefault(__webpack_require__(/*! react */ \"./node_modules/react/index.js\"));\nconst App = () => {\n    return (react_1.default.createElement(\"div\", null,\n        react_1.default.createElement(\"h1\", null, \"Just Trying out React and Nestjs\"),\n        react_1.default.createElement(\"p\", null, \"It's a simple app to test out the integration of React and Nestjs\")));\n};\nexports[\"default\"] = App;\n\n\n//# sourceURL=webpack://try-webpack-ts-react-nestjs-gh-page/./src/client/components/App.tsx?");
 
 /***/ }),
 
